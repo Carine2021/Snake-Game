5cfc334f71ec1074ade8bc8ad3afbf05e6f6a3b7
Carine2021 <cawappi@gmail.com>
2021-07-11 10:59:16 +0200

    Refresh Canvas

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-window.onload = function(){
-  var canvas = document.createElement("canvas");
-  canvas.width = 900;
-  canvas.height = 600;
-  canvas.style.border = "2px solid";
-  document.body.appendChild(canvas);
+window.onload = function()
+{
+  var canvas;
+  var ctx;
+  var delay = 100;
+  var xcoord = 0;
+  var ycoord = 0;
+
+  init();
+
+  function init()
+  {
+    canvas = document.createElement("canvas");
+    canvas.width = 900;
+    canvas.height = 600;
+    canvas.style.border = "1px solid";
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+    refreshCanvas();    
+  }
   
-  var ctx = canvas.getContext('2d');
+  function refreshCanvas()
+  {
+    xcoord += 5;
+    ycoord += 5;
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.fillStyle ="#ff0000";
+    ctx.fillRect(xcoord,ycoord,100,50);
+    setTimeout(refreshCanvas,delay);
+  }
+ 
 }
